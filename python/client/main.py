087a60b8a8c49ef448f44ffcf750c758595aa8b1
Edu <eduedem@gift.com>
2023-01-30 18:07:12 +0100

    Usuario: Fecha y localizacion en Valencia aleatoria

--- python/client/main.py
+++ python/client/main.py
@@ -9,7 +9,9 @@ time_lapse=int(os.getenv('TIME_ID'))
 
 def generatedata(type):
     import random
+    from datetime import datetime, timedelta
 
+    #Hora aleatoria
     def random_time():
         hour = random.randint(0, 23)
         minute = random.randint(0, 59)
@@ -18,6 +20,23 @@ def generatedata(type):
 
         print(random_time())
 
+    #Fecha aleatoria [desde hoy + tiempo ejecucion]
+    def random_date():
+        random_year = random.randint(1900, datetime.now().year)
+        random_month = random.randint(1, 12)
+        random_day = random.randint(1, 28)
+        return datetime(random_year, random_month, random_day)
+
+    # Ubicaciones aleatorias de Valencia
+    import random
+
+    def random_location_in_Valencia():
+        lat = 39.4 + random.uniform(-0.5, 0.5)
+        lng = -0.4 + random.uniform(-0.5, 0.5)
+        return (lat, lng)
+
+    random_location_in_Valencia()
+    random_date()
     random_time()
     data={}
     data["userid"]=user_id
