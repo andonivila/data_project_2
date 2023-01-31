18052859fb3aab0a9c02c3e9959be3bc6abdc8d4
andonivila <andonivila@gmail.com>
2023-01-31 11:24:28 +0100

    Update send_pub_shun.py
    Ahora el envio a Pubsub se hace mediante una funcion

--- anto/python/Send_Pub/send_pub_shun.py
+++ anto/python/Send_Pub/send_pub_shun.py
@@ -1,19 +1,21 @@
 from google.cloud import pubsub_v1
 
-# Your Google Cloud project ID
-project_id = "elevated-column-376015"
 
-# The name of the Pub/Sub topic to which you want to publish
-topic_name = "prueba_send_1"
+def send_mensagge (project_id, topic_name):
+    #Escribir el ID del proyecto
+    project_id = "ID->DEL_PROYECTO_AQUI"
 
-# Set up the Pub/Sub client
-publisher = pubsub_v1.PublisherClient()
-topic_path = publisher.topic_path(project_id, topic_name)
+    #Nombre del topic donde queramos publicar
+    topic_name = "TOPIC->DEL_PROYECTO_AQUI"
 
-# Publish the message and wait for it to be completed
-message = "Hello, Pub/Sub2!"
-future = publisher.publish(topic_path, data=message.encode("utf-8"))
-result = future.result()
+    # Preparacion del cliente de Pub/Sub 
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_name)
 
-# The message has been successfully published
-print("Message published with ID: {}".format(result))
+    # Publica el mensaje y esperar a que se complete el proceso
+    message = " " <-- EL MENSAJE AQUI 
+
+    future = publisher.publish(topic_path, data=message.encode("utf-8"))
+    result = future.result()
+
+    return print("Message published with ID: {}".format(result))
