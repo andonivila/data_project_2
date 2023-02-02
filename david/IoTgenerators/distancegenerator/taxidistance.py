a349f9ea5d9b66ad0b5858fc2f99d8c7be5b8c00
David Moreno <damomar73@gmail.com>
2023-02-02 17:55:38 +0100

    Update schema & generator

--- david/IoTgenerators/distancegenerator/taxidistance.py
+++ david/IoTgenerators/distancegenerator/taxidistance.py
@@ -3,14 +3,14 @@ import requests
 from google.cloud import bigquery
 from google.oauth2.service_account import Credentials
 
-# Autentica con BigQuery usando las credenciales de la cuenta de servicio
+# Autentificamos con BigQuery usando las credenciales de la cuenta de servicio
 credentials = Credentials.from_service_account_file("path/to/service_account.json")
-project_id = "our-project-id"
+project_id = "data-project-2-376316"
 client = bigquery.Client(credentials=credentials, project=project_id)
 
-# Define la consulta SQL para obtener las coordenadas de origen y destino desde BigQuery
+# Crea la consulta SQL para obtener las coordenadas de origen y destino desde BigQuery
 query = """
-SELECT origin_lat, origin_lng, dest_lat, dest_lng
+SELECT Taxi_lat, taxi_lng, User_lat, User_lng
 FROM 'our-dataset.your-table'
 WHERE id = 1
 """
@@ -19,21 +19,14 @@ WHERE id = 1
 query_job = client.query(query)
 rows = query_job.result()
 
-# Obtiene las coordenadas de origen y destino
-origin = [row.origin_lat, row.origin_lng]
-destination = [row.dest_lat, row.dest_lng]
+# Especifica las coordenadas de origen y destino
+origins = "row.Taxi_lat, row.Taxi_lng";
+destinations = "row.User_lat, row.User_lng";
 
-####################################################################################################
-
-# Introducimos API_KEY de Google Maps
+# Se introduce la API_KEY de Google Maps
 
 API_KEY = 'AIzaSyBMazxFGKqM5rDVWyDiFSpESzqjLNgjY4U'  
-
 url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins={origins}&destinations={destinations}&key={API_KEY}"
-
-# Especifica las coordenadas de origen y destino
-origins = "row.origin_lat, row.origin_lng";
-destinations = "row.dest_lat, row.dest_lng";
 
 # Reemplaza las coordenadas en la URL
 url = url.format(origins=origins, destinations=destinations, API_KEY="{API_KEY}")
@@ -41,14 +34,17 @@ url = url.format(origins=origins, destinations=destinations, API_KEY="{API_KEY}"
 # Realiza una solicitud a la API de Google Maps
 response = requests.get(url)
 
-# Verifica si la solicitud es exitosa
+# Verifica si la solicitud es correcta
 if response.status_code == 200:
     # Convierte la respuesta a un diccionario de Python
     data = response.json()
-    # Obtén la distancia en metros
+
+    # Obtiene la distancia en metros
     distance = data["rows"][0]["elements"][0]["distance"]["value"]
+
     # Imprime la distancia
     print("La distancia es de {} metros".format(distance))
+
 else:
     # Imprime un mensaje de error si la solicitud falla
     print("Error al hacer una solicitud a la API de Google Maps")
