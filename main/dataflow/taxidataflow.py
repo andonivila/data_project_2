29f208a602b4f4f5de4a188338a77cb21b69573b
Fran <fran.rosillo.10@gmail.com>
2023-02-08 12:26:48 +0100

    code

--- main/dataflow/taxidataflow.py
+++ main/dataflow/taxidataflow.py
@@ -57,6 +57,7 @@ class MatchShortestDistance(beam.PTransform):
                 |"Group by user_id" >> beam.GroupByKey()
                 | "Find shortest distance" >> beam.Map(lambda x: {
                     'user_id': x[0],
+                    #Aqui podemos ir sacando los campos que queramos de la PColl inicial
                     'taxi_id': min(x[1], key=lambda y: y['init_distance'])['taxi_id'],
                     'calculate shortest_distance': min(x[1], key=lambda y: y['init_distance'])['init_distance'],
                     'calculate final distance': min(x[1], key=lambda y: y['init_distance'])['final_distance']
