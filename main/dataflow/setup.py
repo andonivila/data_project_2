10a45fae7fb4fc87e6cb9d900ea9cfef93ac4005
Fran <fran.rosillo.10@gmail.com>
2023-02-14 13:35:14 +0100

    code

--- main/dataflow/setup.py
+++ main/dataflow/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my-dataflow-job',
+    name='dataflowjobtest',
     version='1.0',
     packages=find_packages(),
     install_requires=[
