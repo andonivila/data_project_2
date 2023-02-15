243305e173ad6ef23ad91646c39978f69fef7807
Fran <fran.rosillo.10@gmail.com>
2023-02-15 10:57:55 +0100

    change time.sleep

--- main/python/user/user.py
+++ main/python/user/user.py
@@ -82,7 +82,7 @@ def senddata(project_id, topic_name):
             pubsub_class.publishMessages(message)
 
             #it will be generated a transaction each 10 seconds
-            time.sleep(random.randint(5, 15))
+            time.sleep(random.randint(5, 25))
     except Exception as err:
         logging.error("Error while inserting data into out PubSub Topic: %s", err)
     finally:
